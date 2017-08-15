bbeda03021c5087151b8c463dd41fcf3e02c5dc4
moyzhong <moyzhong@gmail.com>
2017-08-14 17:02:21 -0700

    Update polarGame.js

--- SadSnake/java/polarGame.js
+++ SadSnake/java/polarGame.js
@@ -2,15 +2,18 @@
   Create Phaser.Game object, maps to gameDiv
   Adds states
   starts boot state */
-
-  var game = new Phaser.Game(640, 480, Phaser.AUTO, 'gameDiv');
-
+alert('game');
+  var game = new Phaser.Game(640, 480, Phaser.CANVAS, 'gameDiv', { preload: preload, create: create, update: update, render: render });
+//  var game = new Phaser.Game(640, 480, Phaser.AUTO, 'gameDiv');
   //Adding each state
-//  game.state.add('boot', bootState);
-//  game.state.add('load', loadState);
-  game.state.add('menu', menuState);
+  game.state.add('boot', bootState);
+  game.state.add('load', loadState);
+//  game.state.add('menu', menuState);
   game.state.add('play', playState);
+  alert('This is okay');
+
   game.state.add('win', winState);
 
+
 //Call the boot state
-  game.state.start('menu');
+  game.state.start('boot');
